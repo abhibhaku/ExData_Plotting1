2ffcbacf7497b86d371a3ac47d0b3561894cefd5
abhibhaku <34106522+abhibhaku@users.noreply.github.com>
2017-12-17 13:52:22 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 #set working directory
 setwd("D:/ISB Co 2018/Coursera/Data Science Specialization/Exploratory Data Analysis/Individual Household Power Consumption Set")
-#read the household power conssumption .txt file & plot the histogram
+#read the household power conssumption .txt file & plot the chart
 house_power <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 house_power_1 <- house_power[house_power$Date %in% c("1/2/2007","2/2/2007") ,]
 house_power_time <- strptime(paste(house_power_1$Date, house_power_1$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
